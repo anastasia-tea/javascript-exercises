4469613c804f4ead8cd49f1b0ccbec3b07c74959
anastasia-tea <anastasia.chapinfortin@gmail.com>
2023-07-19 15:57:44 -0500

    Completed JS exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+
+const repeatString = function(string, num) {
+
+    let str = "";
+
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < num; i++) {
+        str = str + string;
+    }
+
+    return str;
 
 };
 
