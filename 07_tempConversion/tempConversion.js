be1195100d8f06f2a18f8fa10ca23ff0a07f4e78
anastasia-tea <anastasia.chapinfortin@gmail.com>
2023-07-19 17:06:52 -0500

    Completed JS exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+
+const convertToCelsius = function(f) {
+
+  let c = (f - 32) * (5 / 9);
+
+  let roundC = Math.round(c * 10) / 10;
+
+  return roundC;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+
+  let f = (c * (9 / 5) + 32);
+
+  let roundF = Math.round(f * 10) / 10;
+
+  return roundF;
+
 };
 
 // Do not edit below this line
