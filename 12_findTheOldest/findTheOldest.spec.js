67fa1c0d2ff2584230124ead5a7ef44a2005c432
anastasia-tea <anastasia.chapinfortin@gmail.com>
2023-07-24 15:53:23 -0500

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.spec.js
+++ 12_findTheOldest/findTheOldest.spec.js
@@ -1,3 +1,4 @@
+
 const findTheOldest = require('./findTheOldest')
 
 describe('findTheOldest', () => {
@@ -21,7 +22,7 @@ describe('findTheOldest', () => {
     ]
     expect(findTheOldest(people).name).toBe('Ray');
   });
-  test.skip('finds the oldest person if someone is still living', () => {
+  test('finds the oldest person if someone is still living', () => {
     const people = [
       {
         name: "Carly",
@@ -40,7 +41,7 @@ describe('findTheOldest', () => {
     ]
     expect(findTheOldest(people).name).toBe('Ray');
   });
-  test.skip('finds the oldest person if the OLDEST is still living', () => {
+  test('finds the oldest person if the OLDEST is still living', () => {
     const people = [
       {
         name: "Carly",
