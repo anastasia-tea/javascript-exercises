cb9cc787681c8d8405e92e79f2ef66dd7fad19df
anastasia-tea <anastasia.chapinfortin@gmail.com>
2023-07-24 15:02:31 -0500

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
 
+const palindromes = function (str) {
+    const string = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return (string.split("").reverse().join("") == string);
 };
 
 // Do not edit below this line
