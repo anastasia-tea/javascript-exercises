b52806f8e8395b408f8d68fe3f3079c0cd1066b2
anastasia-tea <anastasia.chapinfortin@gmail.com>
2023-07-24 15:11:54 -0500

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+
+const fibonacci = function(number) {
+
+    if (number < 0) {
+        return "OOPS";
+    } else if (number === 1) {
+        return 0;
+    } else {
+        let prevprev = 0;
+        let prev = 1;
+
+        for (let i = 2; i <= number; i++) {
+            let curr = prev + prevprev;
+            prevprev = prev;
+            prev = curr;
+        }
+        return prev;
+    }
 
 };
 
